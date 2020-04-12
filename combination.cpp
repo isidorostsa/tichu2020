bf8ca078448a0e33425173fd47ec0219f986a966
Isidoros Tsaousis-Seiras <isidtsao@office365.auth.gr>
2020-04-12 21:34:14 +0300

    allages combination

--- combination.cpp
+++ combination.cpp
@@ -1,3 +1,16 @@
 #include "combination.h"
 
+Combination::Combination(){
+    numberOfCards = 0; 
+}
+
+void Combination::addCard(Card card){
+    cards[numberOfCards++] = card;
+}
+
+Card Combination::getCard(){
+    
+}
+
+
 // TODO: Implement here the methods of Combination
