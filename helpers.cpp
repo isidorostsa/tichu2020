cc77e503daa3866981dcfd40fead7fef910e134b
Isidoros Tsaousis-Seiras <isidtsao@office365.auth.gr>
2020-04-12 22:12:21 +0300

    syntaktika

--- helpers.cpp
+++ helpers.cpp
@@ -4,7 +4,7 @@
 int numberOfPlayersThatHaveNoCardsLeft(Player players[]){
    int count = 0; 
    for(int i = 0; i < 4; i++){
-        if(players[i].hasNoCardsLeft) count ++; 
+        if(players[i].hasNoCardsLeft()) count ++; 
    }
    return count; 
 }
@@ -12,7 +12,7 @@ int numberOfPlayersThatHaveNoCardsLeft(Player players[]){
 bool lastThreePlayersPassedOrHaveNoCardsLeft(Player players[], int playerTurn){
     int count = 0;
     for(int i = 1; i < 4; i++){
-        if(players[(playerTurn + i) % 4].hasNoCardsLeft || players[(playerTurn + i) % 4].hasStatus("PASSED")) count++;
+        if(players[(playerTurn + i) % 4].hasNoCardsLeft() || players[(playerTurn + i) % 4].hasStatus("PASSED")) count++;
     }
     return count == 3; 
 }
