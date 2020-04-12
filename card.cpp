cbb3765c0e31040ffebdafe6b23b213f33429a8d
Isidoros Tsaousis-Seiras <isidtsao@office365.auth.gr>
2020-04-12 20:04:59 +0300

    get/set suit card.cpp

--- card.cpp
+++ card.cpp
@@ -4,4 +4,11 @@ void Card::setValue(int theValue){
     value = theValue;
 }
 
+void Card::setSuit(string theSuit){
+    suit = theSuit;
+}
+
+string Card::getSuit(){
+    return suit;
+}
 // TODO: Implement here the methods of Card
