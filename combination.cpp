9059a0ea8d0cd315640224d637ad5f3e96416bef
AlexPapadakis <62035964+AlexPapadakis@users.noreply.github.com>
2020-04-12 21:54:28 +0300

    Update combination.cpp
    alex comb.cpp

--- combination.cpp
+++ combination.cpp
@@ -1,3 +1,28 @@
 #include "combination.h"
 
 // TODO: Implement here the methods of Combination
+
+Combination::Combination(){
+  numberOfCards = 0;
+}
+
+void Combination::addCard(Card card){
+  numberOfCards += 1;
+  cards[numberofCards-1] = card;
+}
+
+Card Combination::getCard(int index){
+  return cards[index];
+}
+
+int Combination::getValue(){
+  return cards[0].getvalue();
+}
+
+int Combination::getNumberOfCards(){
+  return numberOfCards;
+}
+
+bool Combination::canBePlayed(Combination last){
+  return numberOfCards == last.getnumberOfCards() && cards[0].value >= last.getValue();
+}
