1a19349656118a5c844cab17a0ee6ee8c1ee9e58
Egret11 <v.kapa147@gmail.com>
2020-04-12 20:42:52 +0300

    Changes on Player.cpp

--- player.cpp
+++ player.cpp
@@ -1,6 +1,29 @@
 #include "player.h"
 
-δοκιμη
+void Player::Player(int idx){
+    index = idx;
+}
+
+void Player::setStatus(string newStatus){
+    status = newStatus;
+}
+
+bool Player::hasStatus(string checkStatus){
+    if(status == checkStatus){
+        return true;
+    }
+    return false;
+}
+
+string Player::getName(){
+    return name;
+}
+
+bool Player::hasNoCardsLeft(){
+    // Do it at the end
+}
+
+
 
 
 // TODO: Implement here the methods of Player
