61ec01787d0ff0d52ca413023fa7fe70fad3ba68
Isidoros Tsaousis-Seiras <isidtsao@office365.auth.gr>
2020-04-12 22:28:13 +0300

    aplopoihsh lastthree....

--- helpers.cpp
+++ helpers.cpp
@@ -12,8 +12,8 @@ int numberOfPlayersThatHaveNoCardsLeft(Player players[]){
 bool lastThreePlayersPassedOrHaveNoCardsLeft(Player players[], int playerTurn){
     int count = 0;
     for(int i = 1; i < 4; i++){
-        if(players[(playerTurn + i) % 4].hasNoCardsLeft() || players[(playerTurn + i) % 4].hasStatus("PASSED")) count++;
+        if(i == playerTurn) continue; 
+        if(players[i].hasNoCardsLeft || players[i].hasStatus("PASSED")) count++; 
     }
     return count == 3; 
 }
-// ALLAGH GIA DOKIMH 
