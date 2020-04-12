5e66dfc6ad85003eead23d01e5aac61b4a81c870
Egret11 <62034209+Egret11@users.noreply.github.com>
2020-04-12 19:27:22 +0300

    Delete player.cpp

--- player.cpp
+++
@@ -1,8 +0,0 @@
-#include "player.h"
-
-
-// TODO: Implement here the methods of Player
-// Do not implement the methods addCardToHand, removeCardFromHand, addCardsToBucket
-// createHandCombinations, findNumberOfPlayableCombinations, playAnyCombination, play
-// These are already implemented in file playercombinations.cpp
-// Implement the rest of the methods of Player
