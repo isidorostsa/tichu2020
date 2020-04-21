ad384bd2bafaa3b30d77b94ec3cfc94995d72e06
Isidoros Tsaousis-Seiras <isidtsao@office365.auth.gr>
2020-04-21 15:49:54 +0300

    allcombs

--- handcombinations.cpp
+++ handcombinations.cpp
@@ -2,10 +2,22 @@
 // TODO: Implement here the method createAllCombinations of Hand
 
 void Hand::createAllCombinations(){
-    Card tempHand[14];
-    for(int i = 0; i <= currentNumberOfCards; i++){
-        tempHand[i] = cards[i];
+    for(int startingIndex = 0; startingIndex < currentNumberOfCards; startingIndex ++){ 
+        Combination single = Combination();
+        single.addCard(cards[startingIndex]);
+        allCombinations[allCombinationsSize++] = single;  
+        for(int loopingIndex = startingIndex + 1; loopingIndex < currentNumberOfCards; loopingIndex ++){
+            if(cards[startingIndex].getValue() == cards[loopingIndex].getValue()){
+                Combination doubles = Combination();
+                doubles.addCard(cards[startingIndex]);
+                doubles.addCard(cards[loopingIndex]);
+                allCombinations[allCombinationsSize++] = doubles; 
+            }
+        }
     }
+}
+
+
 //    for(int i = 0; i < currentNumberOfCards; i++){
 //		for(int j = 0; j < currentNumberOfCards - 1 - i; j++){
 //			if(tempHand[j].getValue() > tempHand[j + 1].getValue()){
@@ -15,20 +27,20 @@ void Hand::createAllCombinations(){
 //			}
 //		}
 //	}
-	for(int i = 0; i < currentNumberOfCards; i++){
-        Combination single = Combination();
-        allCombinations[allCombinationsSize++] = single;
-        single.addCard(tempHand[i]);
-        for(int j = 0; j < currentNumberOfCards; j++){
-            if(tempHand[j].canBeInCombination() == false) continue; 
-            if(tempHand[i].getValue() == tempHand[j].getValue()){
-                Combination doubles = Combination();
-                doubles.addCard(cards[i]);
-				doubles.addCard(cards[j]);
-                allCombinations[allCombinationsSize++] = doubles;
-            }
-        }
-        
-    }
-}
+//	for(int i = 0; i < currentNumberOfCards; i++){
+//        Combination single = Combination();
+//        allCombinations[allCombinationsSize++] = single;
+//        single.addCard(tempHand[i]);
+//        for(int j = 0; j < currentNumberOfCards; j++){
+//            if(tempHand[j].canBeInCombination() == false) continue; 
+//            if(tempHand[i].getValue() == tempHand[j].getValue()){
+//                Combination doubles = Combination();
+//                doubles.addCard(cards[i]);
+//				doubles.addCard(cards[j]);
+//                allCombinations[allCombinationsSize++] = doubles;
+//            }
+//        }
+//        
+//    }
+//}
 
